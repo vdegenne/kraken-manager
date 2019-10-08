8d60317977eb9650ce3add4b40e610e0313a6625
Valentin Degenne <vdegenne@gmail.com>
2019-10-08 15:30:57 +0200

    icons + ripple + dialogs

--- kraken-manager.ts
+++ kraken-manager.ts
@@ -1,10 +1,14 @@
 import '@webcomponents/custom-elements'
 import '@material/mwc-snackbar'
+import 'mwc-app-dialog'
+import '@material/mwc-icon'
+import '@material/mwc-ripple'
 import { html, render } from 'lit-html'
 import { Snackbar } from '@material/mwc-snackbar'
+import { MwcAppDialog } from 'mwc-app-dialog'
 
 /* Selectors */
-const AMOUNT_SELECTOR = '.formatted-currency'
+const AMOUNTS_SELECTOR = '.formatted-currency'
 const ORDER_TYPE_SELECTOR = '.ib.btn-group.top'
 const ORDER_FORM_SELECTOR = '.form-order-simple'
 const VALUE_MONO_SELECTOR = '.val.mono'
@@ -29,9 +33,11 @@ let quickpairs = localStorage.getItem('quickpairs')
   ? JSON.parse(<string>localStorage.getItem('quickpairs'))
   : ['XBT/EUR', 'ETH/EUR', 'XMR/EUR', 'ATOM/EUR', 'XTZ/EUR', 'XRP/EUR']
 let transactionValues: Transaction | null
+declare let chrome: any
 declare let quickPairAccess: HTMLDivElement
 declare let transactionValuesButtons: HTMLDivElement
 declare let snackbar: Snackbar
+declare let dialog: MwcAppDialog
 
 /**
  * On page change
@@ -75,10 +81,28 @@ const pageChange = () => {
  * First time execution
  */
 window.onload = () => {
-  /* Amount element */
-  $(AMOUNT_SELECTOR).css('cursor', 'pointer')
-  // on amount element click
-  $(AMOUNT_SELECTOR).on('click', e => {
+  /* icons */
+  $(`
+  <style>
+    @font-face {
+      font-family: 'Material Icons';
+      font-style: normal;
+      font-weight: 400;
+      src: url(${chrome.runtime.getURL('fonts/MaterialIcons-Regular.eot')}); /* For IE6-8 */
+      src: local('Material Icons'),
+      local('MaterialIcons-Regular'),
+      url(${chrome.runtime.getURL('fonts/MaterialIcons-Regular.woff2')}) format('woff2'),
+      url(${chrome.runtime.getURL('fonts/MaterialIcons-Regular.woff')}) format('woff'),
+      url(${chrome.runtime.getURL('fonts/MaterialIcons-Regular.ttf')}) format('truetype');
+    }
+  </style>
+  `)
+
+  /* Amount elements */
+  $(AMOUNTS_SELECTOR).css('cursor', 'pointer')
+  // on amount (cryptomoney) element click
+  // @ts-ignore
+  document.querySelector(AMOUNTS_SELECTOR).addEventListener('click', e => {
     if (hash.tab === 'new-order') {
       const orderType = getOrderType()
       if (orderType) {
@@ -86,10 +110,33 @@ window.onload = () => {
           case ORDER_TYPE_BUY:
           // no difference for now
           case ORDER_TYPE_SELL:
-            const form = $(ORDER_FORM_SELECTOR)[0]
+            const form: any = $(ORDER_FORM_SELECTOR)[0]
             if (form) {
               // @ts-ignore
-              form.volume.value = [...e.target.childNodes].filter(node => node.nodeType === 3)[1].textContent.trim()
+              form.volume.value = parseFloat(e.target.textContent.trim().slice(1))
+              // doesn't work ?
+              notifyInputElement(form.volume)
+            }
+            break
+        }
+      }
+    }
+  })
+  // on amount (euro) element click
+  document.querySelectorAll(AMOUNTS_SELECTOR)[1].addEventListener('click', e => {
+    if (hash.tab === 'new-order') {
+      const orderType = getOrderType()
+      if (orderType) {
+        switch (orderType) {
+          case ORDER_TYPE_BUY:
+          // no difference for now
+          case ORDER_TYPE_SELL:
+            const form: any = $(ORDER_FORM_SELECTOR)[0]
+            if (form) {
+              // @ts-ignore
+              form.total.value = parseFloat(e.target.textContent.trim().slice(1))
+              // doesn't work ?
+              notifyInputElement(form.total)
             }
             break
         }
@@ -127,6 +174,9 @@ window.onload = () => {
   /* Snackbar */
   $('<mwc-snackbar id="snackbar"></mwc-snackbar>').appendTo(document.body)
 
+  /* Dialog */
+  $('<mwc-app-dialog id="dialog"></mwc-app-dialog>').appendTo(document.body)
+
   /* on page change event */
   $(window).on('hashchange', () => {
     pageChange()
@@ -161,18 +211,25 @@ const updateQuickPairAccess = () => {
         display:flex;
         background: black;
       }
-      #quickPairAccess > span {
-        padding: 14px;
+      #quickPairAccess > span, #quickPairAccess > mwc-icon {
+        padding: 9px 11px;
         cursor: pointer;
         color: white;
+      }
+      #quickPairAccess > span:first-of-type {
+        padding-left: 20px;
+      }
+      #quickPairAccess mwc-ripple { 
+        --mdc-theme-primary: white;
       }
     </style>
     ${quickpairs.length === 0 ? html`<span>no pairs</span>` : null}
     ${quickpairs.map(
       (pair: string) => html`
-    <span @click="${() => navigateToPair(pair)}">${pair}</span>`
+    <span @click="${() => navigateToPair(pair)}">${pair}<mwc-ripple primary></mwc-ripple></span>`
     )}
-    <span style="font-size:140%">+</span>
+    <mwc-icon @click="${openAddQuickPairDialog}">add</mwc-icon>
+    <mwc-icon @click="${openDeleteQuickPairDialog}">delete</mwc-icon>
     `,
     quickPairAccess
   )
@@ -226,4 +283,68 @@ const copyToClipboard = (str: string) => {
   el.select()
   document.execCommand('copy')
   document.body.removeChild(el)
+}
+
+/**
+ * Dialogs
+ */
+const openAddQuickPairDialog = async () => {
+  try {
+    await dialog.open(
+      'type a pair in',
+      html`
+      <input type="text" placeholder="example: XBT/EUR" id="pair" style="padding:5px 6px;box-sizing:border-box;width:100%;">
+      `,
+      (dom: HTMLElement) => {
+        // @ts-ignore
+        const pair = dom.pair.value
+
+        if (!verifyPairExist(pair)) {
+          openSnackbar(`the pair "${pair}" doesn't exist`)
+          return
+        }
+        quickpairs.push(pair)
+        localStorage.setItem('quickpairs', JSON.stringify(quickpairs))
+        updateQuickPairAccess()
+      }
+    )
+  } catch (e) {
+    return
+  }
+}
+
+const openDeleteQuickPairDialog = async () => {
+  try {
+    await dialog.open(
+      'type a pair in',
+      html`
+      <input type="text" placeholder="example: XBT/EUR" id="pair" style="padding:5px 6px;box-sizing:border-box;width:100%;">
+    `,
+      (dom: HTMLElement) => {
+        // @ts-ignore
+        const pair = dom.pair.value
+        if (!quickpairs.includes(pair)) {
+          openSnackbar(`the pair "${pair} is not listed`)
+          return
+        }
+        quickpairs.splice(quickpairs.indexOf(pair), 1)
+        localStorage.setItem('quickpairs', JSON.stringify(quickpairs))
+        updateQuickPairAccess()
+      }
+    )
+  } catch (e) {
+    return
+  }
+}
+
+const verifyPairExist = (pair: string) => {
+  const pairElement = $(`a[data-pair-text="${pair}"]`)
+  return pairElement.length === 1
+}
+
+const notifyInputElement = (input: HTMLInputElement) => {
+  const changevent = document.createEvent('Events')
+  changevent.initEvent('change', true, true)
+  input.dispatchEvent(changevent)
+  input.click()
 }
